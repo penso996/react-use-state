814bd8d1d30c4356f2b14787c4e98afa236deed6
penso996 <penso.andrea@outlook.it>
2025-02-17 16:48:17 +0100

    added buttons from array

--- src/page_components/Main.jsx
+++ src/page_components/Main.jsx
@@ -37,11 +37,9 @@ export default function Main() {
     return (
         <main>
             <ul>
-                <li>ok</li>
-                <li>ok</li>
-                <li>ok</li>
-                <li>ok</li>
-                <li>ok</li>
+                {languages.map((language) => (
+                    <li key={language.id}>{language.title}</li>
+                ))}
             </ul>
         </main>
     );
